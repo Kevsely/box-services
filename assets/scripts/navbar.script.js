06e0092cdd2f31bc963ec4c5b7bc0e47c00b22f9
Kevsely <elegis.sossou@yahoo.com>
2022-06-19 20:21:33 +0100

    Added header and footer on the container page

--- assets/scripts/navbar.script.js
+++ assets/scripts/navbar.script.js
@@ -3,15 +3,10 @@ var navbar = document.getElementsByTagName("nav")[0];
 
 // When the user scrolls down, XXpx from the top of the document (then 650px), change the navbar color and shadow
 function scrollFunction() {
-    if (document.documentElement.scrollTop > 15 && document.documentElement.scrollTop <= 600) {
+    if (document.documentElement.scrollTop > 15 ) {
         navbar.style.boxShadow = "0 3px 5px rgba(0, 0, 0, 0.4)"
-        navbar.style.backgroundColor = "rgba(2, 86, 122, 0.322)"
-    } else if (document.documentElement.scrollTop > 600) {
-        navbar.style.boxShadow = "0 3px 5px rgba(0, 0, 0, 0.4)"
-        navbar.style.backgroundColor = "rgba(2, 86, 122, 0.8)"
     } else {
         navbar.style.boxShadow = "none"
-        navbar.style.backgroundColor = "transparent"
     }
 }
 
