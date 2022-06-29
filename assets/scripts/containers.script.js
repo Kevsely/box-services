93be58797ffe57b00b6311b28644f15c5e5c85b8
Kevsely <elegis.sossou@yahoo.com>
2022-06-29 20:17:51 +0100

    Wrote the addToCart function

--- assets/scripts/containers.script.js
+++ assets/scripts/containers.script.js
@@ -33,9 +33,50 @@ function filterSelection(productFilter) {
         hideNonFilteredElements(resultsToFilter[i], "show");
         if (resultsToFilter[i].className.indexOf(productFilter) > -1) {
             showFilteredElements(resultsToFilter[i], "show");
-            console.log(resultsToFilter[i], "has been showned")
         }
     }
+}
+
+function addToCart(productTileElt) {
+    var cart, existingItem = false, existingItemIndex = -1
+
+    // Children list
+    // 0. price
+    // 1. preview
+    // 2. designation
+    // 3. quantity
+
+    // Extract item elt from DOM
+    const itemToAdd =  {
+        preview: productTileElt.children[1].src,
+        designation: productTileElt.children[2].textContent,
+        unitPrice: +productTileElt.children[0].getAttribute("price"),
+        quantity: +productTileElt.children[3].firstElementChild.value
+    }
+    
+    // Prepare an internal cart
+    if (window.localStorage.getItem("cart")) {
+        cart = JSON.parse(window.localStorage.getItem("cart"))
+        // Check if the item exists in the current cart
+        for (let i = 0; i < cart.length; i++) {
+            if (cart[i].designation === itemToAdd.designation) {
+                existingItem = true
+                existingItemIndex = i
+                break
+            }
+        }
+
+        if (existingItem) {
+            cart[existingItemIndex].quantity += itemToAdd.quantity
+        } else {
+            cart.push(itemToAdd)
+        }
+    } else {
+        cart = [itemToAdd]
+    }
+
+    // ...then add it localStorage
+    window.localStorage.setItem("cart", JSON.stringify(cart))
 }
 
 // Event listeners
@@ -45,6 +86,8 @@ inputCtnType.forEach((ctnTypeRadio) => {
         if (ctnTypeRadio.checked) filterSelection(ctnTypeRadio.value)
     })
 })
+
+
 
 // On load
 filterSelection("all")
